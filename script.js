96ce848508743ffcc284ad06d8ca584e68a7c100
pankaj-2503 <90036864+pankaj-2503@users.noreply.github.com>
2022-12-25 22:06:07 +0530

    Add files via upload
    Just a small clone project of flipkart

---
+++ script.js
@@ -0,0 +1,27 @@
+let input_search=document.getElementById("search_input")
+let form_search=document.getElementById("search_form")
+let recent_searchEle=docuement.querySelector(".recent-search")
+
+let recentArray=["mobile","shoes"]
+
+form_search.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    recentArray.unshift(input_search.value)
+    console.log(recentArray)
+    renderRecent()
+})
+
+function renderRecent() {
+    let recent_Search_html = ''
+    recentArray.forEach(el => {
+        recent_Search_html += `
+        <div class="recent_list">
+            <i class="fa-solid fa-clock-rotate-left"></i>
+            <p>${el}</p>
+        </div>
+    `
+    })
+
+    recent_SearchEl.innerHTML = recent_Search_html;
+}
+renderRecent()
